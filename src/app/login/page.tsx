3606528af5e915cddb1c3ced3b7b7da4ac7b3525
Caio <caio.vr12@gmail.com>
2025-09-24 16:19:51 -0300

    iniciando desenvolvimento da página de login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import { Login } from '@/components/login/Login';
+
+const LoginPage = () => {
+  return <>
+    <div className='containerLogin'>
+      <Login/>
+    </div>
+  </>
+}
+export default LoginPage;
---
+++ src/components/login/Login.tsx
@@ -0,0 +1,9 @@
+import styles from './Login.module.css';
+
+export const Login = () => {
+  return(
+    <div className={styles.loginContainer}>
+      Hello Login
+    </div>
+  );
+};

e3d4a9cde09c4d2e1509275828a0b5bf4ecf8371
Caio <caio.vr12@gmail.com>
2025-09-24 09:55:19 -0300

    desenvolvendo o componente Footer

--- src/components/main/footer/Footer.tsx
+++ src/components/main/footer/Footer.tsx
@@ -1,6 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright } from '@fortawesome/free-solid-svg-icons';
 import styles from './Footer.module.css';
 
 export const Footer = () => 
   <footer className={styles.foot}>
-    Footer content
+    <div className={styles.footTitle}>
+      <h2>Developed by</h2>
+      <div className={styles.copy}>
+        <p>Caio Vinicius Rodrigues</p>
+        <FontAwesomeIcon icon={faCopyright}/>
+      </div>
+    </div>
   </footer>

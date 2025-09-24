535256eb2e2a49b1a2deae3da7d409ea60bba2b8
Caio <caio.vr12@gmail.com>
2025-09-24 16:42:17 -0300

    img de background em Login; desenvolvimento do bloco superior da página Login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,8 +1,10 @@
 import styles from './Login.module.css';
+import { TopSec } from './TopSec/TopSec';
 
 export const Login = () => {
   return(
     <div className={styles.loginContainer}>
+      <TopSec/>
       Hello Login
     </div>
   );
---
+++ src/components/login/TopSec/TopSec.tsx
@@ -0,0 +1,10 @@
+import styles from './TopSec.module.css';
+
+export const TopSec = () => 
+  <section className={`${styles.secTop}`}>
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis odio explicabo placeat eos accusantium nam?
+      </div>
+    </div>
+  </section>

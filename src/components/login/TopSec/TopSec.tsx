c94af786fadafe1ded2465cd246cdfb047b59ff1
Caio <caio.vr12@gmail.com>
2025-09-24 18:24:57 -0300

    concluindo componente do topo da página Login

--- src/components/login/TopSec/TopSec.tsx
+++ src/components/login/TopSec/TopSec.tsx
@@ -1,10 +1,16 @@
+import { LastElement } from './last-element/LastElement';
+import { CenterElements } from './center-elements/CenterElements';
+import { FirstElement } from './first-element/FirstElement';
 import styles from './TopSec.module.css';
 
 export const TopSec = () => 
   <section className={`${styles.secTop}`}>
-    <div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis odio explicabo placeat eos accusantium nam?
+    <div className={styles.containerTopElements}>
+      <span className={styles.spanNone}>Caio V. Rodrigues Systems</span>
+      <div className={`${styles.wrapTopElements}`}>
+        <FirstElement/>
+        <CenterElements/>
+        <LastElement/>
       </div>
     </div>
   </section>

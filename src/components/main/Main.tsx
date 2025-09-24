13910663435e5503bf59d98746c62b17cf99674b
Caio <caio.vr12@gmail.com>
2025-09-24 13:06:03 -0300

    desenvolvimento do componente Specialties dentro do conteúdo principal

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,9 +1,11 @@
 import { Description } from './description/Description';
 import styles from './Main.module.css';
+import { Specialties } from './specialties/Specialties';
 import { Welcome } from './welcome/Welcome';
 
 export const Main = () => 
   <main className={styles.main}>
     <Welcome/>
     <Description/>
+    <Specialties/>
   </main>
---
+++ src/components/main/specialties/Specialties.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faJava, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faLeftRight } from '@fortawesome/free-solid-svg-icons';
+import styles from './Specialties.module.css';
+
+export const Specialties = () => {
+  const [isJavaRight, setIsJavaRight] = useState<boolean>(true);
+
+  return(
+    <section className={`${styles.wrapSpecialties}`}>
+      <div className={`${styles.blockSpecialt} ${styles.blockSpecialtTool}`}>
+        <FontAwesomeIcon  icon={isJavaRight ? faReact : faJava}/>
+      </div>
+      <div className={`${styles.blockSpecialt} ${styles.blockSpecialtCenter}`}>
+        <button onClick={()=>setIsJavaRight(!isJavaRight)}>
+          <FontAwesomeIcon icon={faLeftRight}/>
+        </button>
+      </div>
+      <div className={`${styles.blockSpecialt} ${styles.blockSpecialtTool}`}>
+        <FontAwesomeIcon icon={isJavaRight ? faJava : faReact}/>
+      </div>
+    </section>
+  );
+}

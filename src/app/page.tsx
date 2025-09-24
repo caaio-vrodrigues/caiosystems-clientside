9b2575ead367afd26a855381cea07eab091f5810
Caio <caio.vr12@gmail.com>
2025-09-24 10:21:04 -0300

    desenvolvendo o componente Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Footer } from "@/components/main/footer/Footer";
 import { Header } from "@/components/main/header/Header";
 import { Main } from "@/components/main/Main";
--- src/components/main/header/Header.tsx
+++ src/components/main/header/Header.tsx
@@ -1,5 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAppleAlt, faClose } from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.module.css';
 
-export const Header = () => 
-  <header>
-    Header content
-  </header>
+export const Header = () => {
+  return(
+    <header className={styles.header}>
+      <div className={`${styles.leftBlock} ${styles.block}`}>
+        <FontAwesomeIcon icon={faAppleAlt}/>
+      </div>
+      <div className={`${styles.centerBlock} ${styles.block}`}>
+        <h1>Caio V. Rodrigues Systems</h1>
+      </div>
+      <div className={`${styles.rightBlock} ${styles.block}`} >
+        <span onClick={()=>{}}>
+          <FontAwesomeIcon icon={faClose}/>
+        </span>
+      </div>
+    </header>
+  );
+}

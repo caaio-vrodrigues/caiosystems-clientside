6238b7e54f40e7aa4b8831fca3c8fff588b8c11f
Caio <caio.vr12@gmail.com>
2025-09-24 18:51:26 -0300

    ajustando pasta do componente TopSec; desenvolvimento do bloco inferior da página de Login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,10 +1,13 @@
+import { BottomSec } from './bottom-sec/BottomSec';
 import styles from './Login.module.css';
-import { TopSec } from './TopSec/TopSec';
+import { TopSec } from './top-sec/TopSec';
 
 export const Login = () => {
   return(
     <div className={styles.loginContainer}>
       <TopSec/>
+      <div className={styles.form}>Formulário</div>
+      <BottomSec/>
     </div>
   );
 };

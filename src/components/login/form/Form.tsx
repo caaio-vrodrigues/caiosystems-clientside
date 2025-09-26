20688684ee45ff75526eb4636f8a7344d7d4692e
Caio <caio.vr12@gmail.com>
2025-09-26 12:36:35 -0300

    testando primeira conexão com backend enviando dados via formulário

--- src/components/login/form/Form.tsx
+++ src/components/login/form/Form.tsx
@@ -1,14 +1,48 @@
 'use client';
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouseUser } from '@fortawesome/free-solid-svg-icons';
 import styles from './Form.module.css';
 
 export const Form = () => {
+  const [email, setEmail] = useState('caio@12');
+  const [password, setPassword] = useState('12345678');
+  const router = useRouter();
+
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+      const response = await fetch(`${SERVER_URL}/user/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          username: email,
+          password: password,
+        }).toString(),
+      });
+
+      if (response.ok) {
+        const isLoggedIn = await response.json();
+        if (isLoggedIn) router.push('/');
+        router.push('/login');
+      } 
+      console.error(`Falha no login, dados inválidos`);
+    } catch (error) {
+      console.error('Erro na requisição:', error);
+    }
+  };
+
   return <>
     <div className={styles.loginTitle}>
       <h1>Login</h1>
     </div>
-    <form className={styles.formLogin} onSubmit={()=>{}}>
+    <form className={styles.formLogin} onSubmit={handleLogin}>
       <div className={`${styles.wrapInput} ${styles.wrapInputEmail}`}>
         <input
           type="email"
@@ -16,8 +50,8 @@ export const Form = () => {
           name="email"
           autoComplete="username"
           required
-          value={""}
-          onChange={()=>{}}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div className={`${styles.wrapInput} ${styles.wrapInputPassword}`}>
@@ -27,18 +61,17 @@ export const Form = () => {
           name="password"
           autoComplete="current-password"
           required
-          value={""}
-          onChange={()=>{}}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className={styles.passwordInput}
         />
-        <button 
-          type="submit" 
-          className={styles.submitBtn} 
-          disabled={false}
+        <button
+          type="submit"
+          className={styles.submitBtn}
         >
-          <span>entrar</span><FontAwesomeIcon icon={faHouseUser}/>
+          <FontAwesomeIcon icon={faHouseUser} />
         </button>
       </div>
     </form>
   </>
-}
+};
